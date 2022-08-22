7ddfd8e23c5888ce4cc1a42e33447a1a8af6e4e1
samuel <samuelarandia@gmail.com>
2022-08-21 20:20:33 -0400

    Fixes Deploy server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 // server.js
 const express = require('express');
-const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 
 const app = express();
 app.use(history());
-app.use(serveStatic(`${__dirname}/`));
 const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`server started ${port}`);
