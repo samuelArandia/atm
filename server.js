40b2bf7eafb8a8f617e4a7b879a93c40abaea65c
samuel <samuelarandia@gmail.com>
2022-08-24 08:24:57 -0400

    Fixes Deploy

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
 const express = require('express');
+const path = require('path'); // eslint-disable-line 
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 
 const app = express();
-app.set('port', process.env.PORT || 8080);
 app.use(history());
 app.use(serveStatic(`${__dirname}/dist`));
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port);
 console.log(`server started ${port}`);
+
